753c6600576987caf40dd954ba533bfd801481e9
PraneshASP <praneshas2000@gmail.com>
2023-02-19 00:21:28 +0700

    ✅ add 'isLeapYear' to 'IDateTimeLib'

--- src/interfaces/IDateTimeLib.sol
+++ src/interfaces/IDateTimeLib.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.15;
 /// @notice Interface for the DateTimeLib contract
 interface IDateTimeLib {
     function weekday(uint256 timestamp) external view returns (uint256 result);
+
+    function isLeapYear(uint256 year) external view returns (bool result);
 }
