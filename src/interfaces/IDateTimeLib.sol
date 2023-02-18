c35f591a471976f998613a26b7a9b7e8ffd26038
PraneshASP <praneshas2000@gmail.com>
2023-02-19 00:09:21 +0700

    ✨ add interface for 'DateTimeLib'

---
+++ src/interfaces/IDateTimeLib.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+/// @notice Interface for the DateTimeLib contract
+interface IDateTimeLib {
+    function weekday(uint256 timestamp) external view returns (uint256 result);
+}
