20a5f38829e3f847eea1855e4057496c46f0a96f
PraneshASP <praneshas2000@gmail.com>
2023-02-23 00:52:11 +0700

    🧪 add tests for 'timestampToDate()'

--- test/DateTimeLib.t.sol
+++ test/DateTimeLib.t.sol
@@ -236,8 +236,171 @@ contract DateTimeLibTest is Test {
     }
 
     function testEpochDayToDate(uint256 epochDay) public {
+        // TODO: Need to check why it randomly fails on certain inputs
         DateTime memory d;
         (d.year, d.month, d.day) = sut.epochDayToDate(epochDay);
-        assertEq(epochDay, sut.dateToEpochDay(d.year, d.month, d.day));
+        assertEq(
+            epochDay,
+            sut.dateToEpochDay(d.year, d.month, d.day),
+            "ValidateEpochDay"
+        );
+    }
+
+    function testDateToAndFroTimestamp() public {
+        unchecked {
+            for (uint256 i; i < 250; ++i) {
+                uint256 year = _bound(_random(), 1970, MAX_SUPPORTED_YEAR);
+                uint256 month = _bound(_random(), 1, 12);
+                uint256 md = sut.daysInMonth(year, month);
+                uint256 day = _bound(_random(), 1, md);
+                uint256 timestamp = sut.dateToTimestamp(year, month, day);
+                assertEq(
+                    timestamp,
+                    sut.dateToEpochDay(year, month, day) * 86400
+                );
+                (uint256 y, uint256 m, uint256 d) = sut.timestampToDate(
+                    timestamp
+                );
+
+                assertEq(year, y, "year");
+                assertEq(month, m, "month");
+                assertEq(day, d, "day");
+            }
+        }
+    }
+
+    function _random() internal returns (uint256 r) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // This is the keccak256 of a very long string I randomly mashed on my keyboard.
+            let
+                sSlot
+            := 0xd715531fe383f818c5f158c342925dcf01b954d24678ada4d07c36af0f20e1ee
+            let sValue := sload(sSlot)
+
+            mstore(0x20, sValue)
+            r := keccak256(0x20, 0x40)
+
+            // If the storage is uninitialized, initialize it to the keccak256 of the calldata.
+            if iszero(sValue) {
+                sValue := sSlot
+                let m := mload(0x40)
+                calldatacopy(m, 0, calldatasize())
+                r := keccak256(m, calldatasize())
+            }
+            sstore(sSlot, add(r, 1))
+
+            // Do some biased sampling for more robust tests.
+            for {
+
+            } 1 {
+
+            } {
+                let d := byte(0, r)
+                // With a 1/256 chance, randomly set `r` to any of 0,1,2.
+                if iszero(d) {
+                    r := and(r, 3)
+                    break
+                }
+                // With a 1/2 chance, set `r` to near a random power of 2.
+                if iszero(and(2, d)) {
+                    // Set `t` either `not(0)` or `xor(sValue, r)`.
+                    let t := xor(
+                        not(0),
+                        mul(iszero(and(4, d)), not(xor(sValue, r)))
+                    )
+                    // Set `r` to `t` shifted left or right by a random multiple of 8.
+                    switch and(8, d)
+                    case 0 {
+                        if iszero(and(16, d)) {
+                            t := 1
+                        }
+                        r := add(
+                            shl(shl(3, and(byte(3, r), 31)), t),
+                            sub(and(r, 7), 3)
+                        )
+                    }
+                    default {
+                        if iszero(and(16, d)) {
+                            t := shl(255, 1)
+                        }
+                        r := add(
+                            shr(shl(3, and(byte(3, r), 31)), t),
+                            sub(and(r, 7), 3)
+                        )
+                    }
+                    // With a 1/2 chance, negate `r`.
+                    if iszero(and(32, d)) {
+                        r := not(r)
+                    }
+                    break
+                }
+                // Otherwise, just set `r` to `xor(sValue, r)`.
+                r := xor(sValue, r)
+                break
+            }
+        }
+    }
+
+    function _bound(
+        uint256 x,
+        uint256 min,
+        uint256 max
+    ) internal pure virtual override returns (uint256 result) {
+        require(
+            min <= max,
+            "_bound(uint256,uint256,uint256): Max is less than min."
+        );
+
+        /// @solidity memory-safe-assembly
+        assembly {
+            for {
+
+            } 1 {
+
+            } {
+                // If `x` is between `min` and `max`, return `x` directly.
+                // This is to ensure that dictionary values
+                // do not get shifted if the min is nonzero.
+                // More info: https://github.com/foundry-rs/forge-std/issues/188
+                if iszero(or(lt(x, min), gt(x, max))) {
+                    result := x
+                    break
+                }
+
+                let size := add(sub(max, min), 1)
+                if and(iszero(gt(x, 3)), gt(size, x)) {
+                    result := add(min, x)
+                    break
+                }
+
+                let w := not(0)
+                if and(iszero(lt(x, sub(0, 4))), gt(size, sub(w, x))) {
+                    result := sub(max, sub(w, x))
+                    break
+                }
+
+                // Otherwise, wrap x into the range [min, max],
+                // i.e. the range is inclusive.
+                if iszero(lt(x, max)) {
+                    let d := sub(x, max)
+                    let r := mod(d, size)
+                    if iszero(r) {
+                        result := max
+                        break
+                    }
+                    result := add(add(min, r), w)
+                    break
+                }
+                let d := sub(min, x)
+                let r := mod(d, size)
+                if iszero(r) {
+                    result := min
+                    break
+                }
+                result := add(sub(max, r), 1)
+                break
+            }
+        }
     }
 }
